9336ab5f73539fa1c902983d130224dde69a16ae
JaviB10 <javi4195@gmail.com>
2020-10-23 14:37:58 -0300

    Agregamos el metodo __str__

--- clienteCorporativo.py
+++ clienteCorporativo.py
@@ -10,3 +10,8 @@ class ClienteCorporativo(Cliente):
         self.telefono_contacto = telefono_contacto
         super().__init__(telefono, mail, id_cliente)
          
+    def __str__(self):
+        Cadena = f"{self.id_cliente}:{self.nombre_empresa} (Cliente Corporativo)\n"
+        Cadena+= f"{self.telefono} - {self.mail}\n"
+        Cadena+= f"Datos del contacto: {self.nombre_contacto} - {self.telefono_contacto}\n"
+        return Cadena
