dd750f4a26063ad2e6b7202976d5d7fd1f49d24f
JaviB10 <javi4195@gmail.com>
2020-10-23 15:05:25 -0300

    Nueva clase ListasClientes

---
+++ Metodosclientes.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+from cliente import Cliente
+from clienteParticular import ClienteParticular
+from clienteCorporativo import ClienteCorporativo
+from repositorioClientes import RepositorioClientes
+
+class ListaClientes:
+    def __init__(self):
+        self.RC = RepositorioClientes()
+        self.ClienteL = self.RC.get_all()
+
+    def NuevoClienteCorp(self, nombre_empresa, nombre_contacto, telefono_contacto, telefono, mail):
+        """Recibe los datos de un cliente corporativo, crea un nuevo cliente corporativo y lo agrega a lista de clientes"""
+        C = ClienteCorporativo(nombre_empresa, nombre_contacto, telefono_contacto, telefono, mail)
+        C.id_cliente = self.RC.store(C)
+        if C.id_cliente == 0:
+            return None
+        else:
+            self.ClienteL.append(C)
+            return C
+
+    def NuevoClientePart(self, nombre, apellido, telefono, mail):
+        """Recibe los datos de un cliente particular, crea un nuevo cliente particular y lo agrega a la lista de clientes"""
+        C = ClienteParticular(nombre, apellido, telefono, mail)
+        C.id_cliente = self.RC.store(C)
+        if C.id_cliente == 0:
+            return None
+        else:
+            self.ClienteL.append(C)
+            return C
+
+    def BuscarPorID(self, id_cliente):
+        """Recibe un ID y retorna un cliente"""
+        for T in self.ClienteL:
+            if T.id_cliente == int(id_cliente):
+                return (T)
+        return None
+
+    def ModificarDatosCP(self, nombre, apellido, telefono ,mail, id_cliente):
+        """Recibe los datos modificados y actualiza los datos del cliente particular"""
+        C = self.BuscarPorID(id_cliente)
+        if C:
+            C.nombre = nombre
+            C.apellido = apellido
+            C.telefono = telefono
+            C.mail = mail
+            return self.RC.update(C)
+        return None
+
+    def ModificarDatosCC(self, nombre_empresa, nombre_contacto, telefono_contacto, telefono, mail, id_cliente):
+        """Recibe los datos modificados y actualiza los datos del cliente corporativo"""
+        C = self.BuscarPorID(id_cliente)
+        if C:
+            C.nombre_empresa = nombre_empresa
+            C.nombre_contacto = nombre_contacto
+            C.telefono_contacto = telefono_contacto
+            C.telefono = telefono
+            C.mail = mail
+            return self.RC.update(C)
+        return None
+
+    def EliminarCliente(self, id_cliente):
+        """"Recibe el ID de un cliente y lo elimina"""
+        C = self.BuscarPorID(id_cliente)
+        if C:
+            return self.RC.delete(C)
+        return None
